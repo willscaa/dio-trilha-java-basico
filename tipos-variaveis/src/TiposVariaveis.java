74ca36a71f6295d6d1193a832b0463cf652649dd
willscaa <willianrocha7854@gmail.com>
2024-05-09 18:55:47 -0300

    Inserção de operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Willian";
+
+        int numero = 5;
+		numero = 10;
+
+		System.out.print(numero);
+		
+		final double VALOR_DE_PI = 3.14;
+		
+	
+        
     }
 }
