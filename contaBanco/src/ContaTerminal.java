cd7d84e4af0270d38ca26cfcf7988ea85c8035d0
willscaa <willianrocha7854@gmail.com>
2024-05-11 21:19:39 -0300

    desafio do projetinho de conta de banco finalizado

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da conta bancária: ");
+        int numeroConta = scanner.nextInt(); 
+
+        System.out.println("Digite o número da agência: ");
+        String agenciaBancaria = scanner.next();
+
+        System.out.println("Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println( "Digite o saldo: " );
+        Double saldoConta = scanner.nextDouble();
+
+        System.out.print("Olá "+nomeCliente+ " muito obrigado por criar uma conta em nosso banco, sua agência é: "+agenciaBancaria+" conta: "+numeroConta+ " e seu saldo de: "+saldoConta+ " que já esta disponível para saque. ");
     }
 }
